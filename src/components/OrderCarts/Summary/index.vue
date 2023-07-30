646252964fc7e026866bce58576062c602d6eb29
fdhhhdjd <nguyentientai10@gmail.com>
2023-07-30 19:02:27 +0700

    #1 #2 #3 product,cart and logic order

--- src/components/OrderCarts/Summary/index.vue
+++ src/components/OrderCarts/Summary/index.vue
@@ -10,13 +10,19 @@ const props = defineProps(['orderItem']);
 
 <template>
   <div class="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6">
-    <h3 class="text-xl font-semibold leading-5 text-gray-800">Summary</h3>
+    <h3 class="text-xl font-semibold leading-5 text-gray-800">
+Summary
+</h3>
     <div
       class="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4"
     >
       <div class="flex justify-between w-full">
-        <p class="text-base leading-4 text-gray-800">Cost Total:</p>
-        <p class="text-base leading-4 text-gray-600">${{ rouserNumber(props.orderItem.cost) }}</p>
+        <p class="text-base leading-4 text-gray-800">
+Cost Total:
+</p>
+        <p class="text-base leading-4 text-gray-600">
+${{ rouserNumber(props.orderItem.cost) }}
+</p>
       </div>
       <div class="flex justify-between items-center w-full">
         <p class="text-base leading-4 text-gray-800">
@@ -26,15 +32,23 @@ const props = defineProps(['orderItem']);
             >STUDENT</span
           >
         </p>
-        <p class="text-base leading-4 text-gray-600">0 (0%)</p>
+        <p class="text-base leading-4 text-gray-600">
+0 (0%)
+</p>
       </div>
       <div class="flex justify-between items-center w-full">
-        <p class="text-base leading-4 text-gray-800">Shipping</p>
-        <p class="text-base leading-4 text-gray-600">$0</p>
+        <p class="text-base leading-4 text-gray-800">
+Shipping
+</p>
+        <p class="text-base leading-4 text-gray-600">
+$0
+</p>
       </div>
     </div>
     <div class="flex justify-between items-center w-full">
-      <p class="text-base font-semibold leading-4 text-gray-800">Total</p>
+      <p class="text-base font-semibold leading-4 text-gray-800">
+Total
+</p>
       <p class="text-base font-semibold leading-4 text-gray-600">
         ${{ rouserNumber(props.orderItem.total) }}
       </p>
--- src/utils/index.js
+++ src/utils/index.js
@@ -73,5 +73,5 @@ export const getURIFromTemplate = (template, data) => {
 };
 
 export const getImage = (pathImage) => {
-  return `/src/assets/${pathImage}`;
+  return `/assets/${pathImage}`;
 };
