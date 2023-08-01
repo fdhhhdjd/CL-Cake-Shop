a078129dc926473b5ab8217cafe9aeee53dbbae0
fdhhhdjd <nguyentientai10@gmail.com>
2023-08-01 13:47:52 +0700

    #1 #2 #3 #4 #5 product,cart and logic order and thankyou

--- src/pages/notfound/index.vue
+++ src/pages/notfound/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white dark:bg-gray-900">
+  <div class="bg-white">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-sm text-center">
         <h1
@@ -7,7 +7,7 @@
         >
           404
         </h1>
-        <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+        <p class="mb-4 text-3xl tracking-tight font-boldmd:text-4xl dark:text-white">
           Something's missing.
         </p>
         <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
@@ -16,10 +16,9 @@
         <RouterLink
           to="/"
           class="inline-flex text-white bg-[#ffa800] hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
-          >
-Back to Homepage
-</RouterLink
         >
+          Back to Homepage
+        </RouterLink>
       </div>
     </div>
   </div>
