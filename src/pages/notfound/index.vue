56fce2560fbd376379970473363735295a293c3c
fdhhhdjd <nguyentientai10@gmail.com>
2023-08-01 13:49:39 +0700

    #1 #2 #3 #4 #5 product,cart and logic order and thankyou

--- src/pages/notfound/index.vue
+++ src/pages/notfound/index.vue
@@ -7,7 +7,7 @@
         >
           404
         </h1>
-        <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+        <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
           Something's missing.
         </p>
         <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
